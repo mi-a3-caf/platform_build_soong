1e92cdefc20f9d7544cdb382065a75f2b42b2e79
Nicholas Lim <nicknitewolf@lineageos.org>
2019-10-30 00:39:37 +0800

    soong: java: Tune java compiler flags for low ram systems
    * Reduce max heap size
    
    Change-Id: I75b052d5bb432bd31644c7c6fe87a3449e593aea

--- java/config/config.go
+++ java/config/config.go
@@ -51,7 +51,7 @@ var (
 func init() {
 	pctx.Import("github.com/google/blueprint/bootstrap")
 
-	pctx.StaticVariable("JavacHeapSize", "2048M")
+	pctx.StaticVariable("JavacHeapSize", "1024M")
 	pctx.StaticVariable("JavacHeapFlags", "-J-Xmx${JavacHeapSize}")
 	pctx.StaticVariable("DexFlags", "-JXX:+TieredCompilation -JXX:TieredStopAtLevel=1")
 
